42a799563bfa1371833ac36860b7fb6a61e40238
Vivian22 <103799715+vivianchen22@users.noreply.github.com>
2022-06-11 15:35:49 +0800

    Create Data_Tidy_Long_and_Wide_Data.R

---
+++ Data_Tidy_Long_and_Wide_Data.R
@@ -0,0 +1,32 @@
+#data tidy from wide data to long data 
+religion_income <- read_delim(
+       "http://stat405.had.co.nz/data/pew.txt",
+      delim = "\t"
+   )
+   
+#way 1 by pivot_longer 
+religion_income %>% pivot_longer(!religion,names_to="income",values_to = "count")
+
+#way 2 by gather
+religion_income %>% gather(key="income",value = "count",!religion)
+
+
+#data tidy from long data to wide data
+if (!file.exists("fishdata.csv")) {
+      download.file(
+           url = 'https://github.com/Myfanwy/ReproducibleExamples/raw/master/encounterhistories/fishdata.csv',
+            destfile = "fishdata.csv"
+        )
+}
+
+fish_encounters <- read_csv("fishdata.csv")
+
+#way 1 by pivot_wider
+fish_encounters %>% pivot_wider(names_from = Station,values_from=value,values_fill =0)
+
+#way 2 by spread
+fish_encounters %>% spread(key=Station,value=value,fill=0)
+
+
+
+
